9eec775b12a8d6daa3dd2a476282b0a2db743b4a
Caleb James <calebsjames@gmail.com>
2021-03-03 10:27:24 -0600

    animal details

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,47 +1,30 @@
-import React, { useContext, useEffect } from "react"
-import { useHistory } from "react-router-dom"
+import React, { useState, useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
-import { LocationContext } from "../locations/LocationProvider"
-import { CustomerContext } from "../customers/CustomerProvider"
 
-export const AnimalList = () => {
-  // This state changes when `getAnimals()` is invoked below
-  const { animals, getAnimals } = useContext(AnimalContext)
-  const { locations, getLocations } = useContext(LocationContext)
-  const { customers, getCustomers } = useContext(CustomerContext)
+export const AnimalList = ({ history }) => {
+    const { getAnimals, animals } = useContext(AnimalContext)
 
-  //useEffect - get a global variable. What do you want and when do you want it
-  //In this case we are getting everything from all the dataProviders we are using
-  //how does the order affect things?
-  useEffect(() => {
-    getLocations()
-    .then(getCustomers)
-    .then(getAnimals)
-  }, [])
+    // Initialization effect hook -> Go get animal data
+    useEffect(()=>{
+        getAnimals()
+    }, [])
 
-  const history = useHistory()
-  
+    return (
+        <>
+            <h1>Animals</h1>
 
-  //return HTML represention
-  return (
-    <>
-      <h2>Animals</h2>
-          <button onClick={() => {history.push("animals/create")}}>
-        Add Animal
-      </button>
-      <div className="animals">
-        {animals.map(animalObject => {
-            const owner = customers.find(c => c.id === animalObject.customerId)
-            const clinic = locations.find(l => l.id === animalObject.locationId)            
-            return <AnimalCard key={animalObject.id}
-                    location = {clinic}
-                    customer = {owner} 
-                    animalInstance={animalObject} />
-          })
-        }
-      </div>
-    </>
-  )
+            <button onClick={() => history.push("/animals/create")}>
+                Make Reservation
+            </button>
+            <div className="animals">
+                {
+                    animals.map(animalObject => {
+                        return <AnimalCard key={animalObject.id} animalInstance={animalObject} />
+                    })
+                }
+            </div>
+        </>
+    )
 }
