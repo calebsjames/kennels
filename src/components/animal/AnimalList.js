05e858e37f4b6b7e44a98e0d105320f201ded910
Caleb James <calebsjames@gmail.com>
2021-03-03 11:33:58 -0600

    Add new animal working again

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,21 +2,25 @@ import React, { useState, useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
+import { useHistory } from "react-router-dom"
+
 
 export const AnimalList = ({ history }) => {
-    const { getAnimals, animals } = useContext(AnimalContext)
-
-    // Initialization effect hook -> Go get animal data
-    useEffect(()=>{
-        getAnimals()
-    }, [])
+  const { getAnimals, animals } = useContext(AnimalContext)
+  
+  // Initialization effect hook -> Go get animal data
+  useEffect(()=>{
+    getAnimals()
+  }, [])
+  
+  history = useHistory()
 
     return (
         <>
             <h1>Animals</h1>
 
             <button onClick={() => history.push("/animals/create")}>
-                Make Reservation
+                New Animal
             </button>
             <div className="animals">
                 {
