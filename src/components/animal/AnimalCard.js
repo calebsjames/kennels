2627ebd89d97d5ee0d886c8aeed8a7e394d3c9ba
Caleb James <calebsjames@gmail.com>
2021-03-03 11:24:11 -0600

    employee details rendering

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -15,3 +15,19 @@ export const AnimalCard = ({ animalInstance }) => (
         <div className="animal__breed">{ animalInstance.breed }</div>
     </section>
 )
+
+
+// import React from "react"
+// import "./Animal.css"
+
+
+// //animalInstance.location.name? Because expand in the provider. Not recommended.
+
+// export const AnimalCard = ({ animalInstance, customer, location }) => (
+//     <section className="animal">
+//         <h3 className="animal__name">{animalInstance.name}</h3>
+        
+//         <div className="location__name">{location.name}</div>
+//         <div className="owner__name">{customer.name}</div>
+//     </section>
+// )
